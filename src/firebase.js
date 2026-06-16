61259279e9ce93053a036cd42dce06a98534f288
unknown <mubashirgenrobotics@gmail.com>
2026-06-16 15:09:45 +0530

    Initial Vite project and Firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAh_IF-Jfml2IlU445PL8RI_kRB1YVjCJU",
+  authDomain: "spare-pro.firebaseapp.com",
+  projectId: "spare-pro",
+  storageBucket: "spare-pro.firebasestorage.app",
+  messagingSenderId: "145184069737",
+  appId: "1:145184069737:web:976e41d40ca08f39356af8",
+  measurementId: "G-9N2T0EDKQH"
+};
